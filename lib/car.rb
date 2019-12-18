b06635768a7fb059692ccef6cce643f41ef04062
Tariq Bailey <tariqbailey627@gmail.com>
2019-12-18 23:31:28 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,6 +2,10 @@ require_relative "./vehicle.rb"
 
 class Car < Vehicle
   def go
+<<<<<<< HEAD
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"  
+=======
+      
+>>>>>>> 8485d9a051f305c2e78c114f35b8fbe41c89314f
   end
 end
